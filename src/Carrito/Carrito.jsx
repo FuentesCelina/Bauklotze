08403bcdb96b375404430e293ea7e85127fee04c
FuentesCelina <fuentescelina01@gmail.com>
2026-05-20 17:14:09 -0300

    se crean nuevos componentes de carrito

---
+++ src/Carrito/Carrito.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const Carrito = () => {
+    return (
+        <>
+            <h1>Tu Carrito</h1>
+            {carrito.map((producto) => (
+                <div className="product-card" key={producto.id}>
+                <img src={producto.image} alt={producto.name} className="picture-card"/>
+                <h2 className="name-card">{producto.name}</h2>
+                <p className="precio-card">{producto.price}</p>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default Carrito;
